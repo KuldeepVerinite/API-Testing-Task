c82661da432ebe641a8e7dc82989c5168a6bc625
Prajyot28 <prajyot.sidwadkar@verinite.com>
2024-07-24 18:05:50 +0530

    All code till 24june

--- src/test/java/Method/PTestClass2307.java
+++ src/test/java/Method/PTestClass2307.java
@@ -1,46 +1,47 @@
 package Method;
 
+import org.testng.annotations.Test;
+
 public class PTestClass2307 {
 
-//    PostService ps = new PostService();
-//
-//    @Test
-//    public void createInvalidBody(){
-//        ps.createBooking("Prajyot", "Sidwadkar", 500);
-//    }
+    PostService ps = new PostService();
+
+    @Test(priority = 1)
+    public void createInvalidBody(){
+        ps.createBooking("Prajyot", "Sidwadkar", 500);
+    }
 
     PutService ps1 = new PutService();
 
-//    @Test
-//    public void updateInvalidBody(){
-//        ps1.updateBooking("Me", "You", 1332, 400, "Basic YWRtaW46cGFzc3dvcmQxMjM=");
-//    }
+    @Test(priority = 2)
+    public void updateInvalidBody(){
+        ps1.updateBooking("Me", "You", 1332, 400, "Basic YWRtaW46cGFzc3dvcmQxMjM=");
+    }
 
-//    @Test
-//    public void updateBookingWithoutAuthHeader(){
-//        ps1.updateBooking("You", "Me", 955, 403, "");
-//    }
+    @Test(priority = 3)
+    public void updateBookingWithoutAuthHeader(){
+        ps1.updateBooking("You", "Me", 955, 403, "");
+    }
 
-//    @Test
-//    public void updateBookingInvalidAuthHeader(){
-//        ps1.updateBooking("You", "Me", 955, 403, "wehfksbfjwejfnwejfwef");
-//    }
+    @Test(priority = 4)
+    public void updateBookingInvalidAuthHeader(){
+        ps1.updateBooking("You", "Me", 955, 403, "wehfksbfjwejfnwejfwef");
+    }
 
     CreateTokenDeleteBooking ctdb = new CreateTokenDeleteBooking();
 
-//    @Test
-//    public void updateBookingCookieheader(){
-//        String token =
-//        ctdb.createToken1("admin", "password123", 200, 12);
-//        ps1.updateBookingCookie("I", "Me", 12, 403, "" );
-//    }
+    @Test(priority = 5)
+    public void updateBookingCookieheader(){
+        String token =
+        ctdb.createToken1("admin", "password123", 200);
+        ps1.updateBookingCookie("I", "Me", 12, 403, "" );
+    }
 
-//    @Test
-//    public void updateBookingInvalidCookieheader(){
-//        String token =
-//                ctdb.createToken1("admin", "password123", 200, 12);
-//        ps1.updateBookingCookie("I", "Me", 12, 403, token+"dfgdfg" );
-//    }
-
+    @Test(priority = 6)
+    public void updateBookingInvalidCookieheader(){
+        String token =
+                ctdb.createToken1("admin", "password123", 200);
+        ps1.updateBookingCookie("I", "Me", 12, 403, token+"dfgdfg" );
+    }
 
 }
--- src/test/java/Method/PTestClass2407.java
+++ src/test/java/Method/PTestClass2407.java
@@ -7,29 +7,19 @@ public class PTestClass2407 {
     PatchService ps = new PatchService();
     CreateTokenDeleteBooking ctdb = new CreateTokenDeleteBooking();
 
-//    @Test
-//    public void PartialUpdateBooking(){
-//        ps.patchUpdateBooking("Sharad", 1230, 200,"Basic YWRtaW46cGFzc3dvcmQxMjM=");
-//    }
+    @Test(priority = 1)
+    public void PartialUpdateBooking(){
+        ps.patchUpdateBooking("Sharad", 1230, 200,"Basic YWRtaW46cGFzc3dvcmQxMjM=");
+    }
 
-//    @Test
-//    public void partialBookingWithoutCookie(){
-//         String token =
-//        ctdb.createToken1("admin", "password123",200);
-//        ps.partialBookingCookie("Prajyot", 765, 403, "" );
-//    }
+    @Test(priority = 2)
+    public void partialBookingWithoutCookie(){
+         String token =
+        ctdb.createToken1("admin", "password123",200);
+        ps.partialBookingCookie("Prajyot", 765, 403, "" );
+    }
 
-//    @Test
-//    public void partialBookingOldCookie(){
-//        String token1=
-//                ctdb.createToken1("admin","password123", 200);
-//        String token2=
-//                ctdb.createToken1("admin","password123", 200);
-//        ps.partialBookingCookie("Prajyot", 378, 200, token1);
-//        ps.partialBookingCookie("Me", 535, 403, token1);
-//    }
-
-    @Test
+    @Test(priority = 3)
     public void partialBookingOldCookie(){
         String token1=
                 ctdb.createToken1("admin","password123", 200);
@@ -37,6 +27,19 @@ public class PTestClass2407 {
                 ctdb.createToken1("admin","password123", 200);
         ps.partialBookingCookie("Prajyot", 378, 200, token1);
         ps.partialBookingCookie("Me", 535, 403, token1);
+    }
+
+
+    @Test(priority = 4)
+    public void partialBookingInvalidCookie(){
+        String token=
+                ctdb.createToken1("admin","password123", 200);
+        ps.partialBookingCookie("Me", 535, 403, token+"sdfg" );
+    }
+
+    @Test(priority = 5)
+    public void partialBookingValidAuth(){
+        ps.patchUpdateBooking("You", 535, 200, "Basic YWRtaW46cGFzc3dvcmQxMjM=");
     }
 
 }
